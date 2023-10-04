c43505f46c6a83ed52d8b7ebfad3242a3e948d5c
Dennis <dennis.spierenburg@hotmail.com>
2023-10-04 09:18:22 +0200

    removed config

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,10 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 };
